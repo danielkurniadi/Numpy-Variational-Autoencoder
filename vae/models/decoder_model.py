b904c83d51fbddb247c82bad7ded079756601aa4
iqDF <daniel.thekurniadi@gmail.com>
2019-12-14 14:00:00 +0100

    decoder backward method

--- vae/models/decoder_model.py
+++ vae/models/decoder_model.py
@@ -51,3 +51,58 @@ class Decoder(object):
         self.d_out = np.reshape(self.h1_a, (self.batch_size, self.input_channels))
 
         return self.d_out
+
+    def optimise(self, grads):
+        """
+        """
+        # ---------------------------
+        # Optimise using Adam
+        # ---------------------------
+        self.t += 1
+        # Calculate gradient with momentum and velocity
+        for i, grad in enumerate(grads):
+            self.momentum[i] = (1 - self.beta1) * grad
+            self.velocity[i] = self.beta2 * self.velocity[i] + (1 - self.beta2) * np.power(grad, 2)
+            m_h = self.momentum[i] / (1 - (self.beta1 ** self.t))
+            v_h = self.velocity[i] /  (1 - (self.beta2 ** self.t))
+            grads[i] = m_h / np.sqrt(v_h + eps)
+
+        grad_dW0, grad_db0, grad_dW1, grad_db1 = grads
+
+        # Update weights
+        self.W0 = self.W0 - self.lr * np.sum(grad_dW0, axis=0)
+        self.b0 = self.b0 - self.lr * np.sum(grad_db0, axis=0)
+        self.W1 = self.W1 - self.lr * np.sum(grad_dW1, axis=0)
+        self.b1 = self.b1 - self.lr * np.sum(grad_db1, axis=0)
+
+        return
+
+    def backward(self, x, out):
+        # ----------------------------------------
+        # Calculate gradients from reconstruction
+        # ----------------------------------------
+        y = np.reshape(x, (self.batch_size, -1))
+        out = np.reshape(out, (self.batch_size, -1))
+
+        dL = MSELoss(out, y, derivative=True)
+        dSig = sigmoid(self.h1_l, derivative=True)
+
+        dL_dSig = dL * dSig
+
+        grad_db1 = dL_dSig
+        grad_dW1 = np.matmul(np.expand_dims(self.h0_a, axis=-1), np.expand_dims(dL_dSig, axis=1))
+        
+        drelu0 = relu(self.h0_l, derivative=True)
+
+        grad_db0 = grad_db1.dot(self.W1.T) * drelu0
+        grad_dW0 = np.matmul(np.expand_dims(self.z, axis=-1), np.expand_dims(grad_db0, axis=1))
+
+        # output gradient to the encoder layer
+        grad_dec = grad_db0.dot(self.W0.T)
+
+        grads = [grad_dW0, grad_db0, grad_dW1, grad_db1]
+
+        # Optimiser Step
+        self.optimise(grads)
+
+        return grad_dec
